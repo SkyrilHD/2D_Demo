89960465dfbb23abf0f8d248db6e418f1e23563e
SkyrilHD <skyrilhd@gmail.com>
2020-02-11 22:16:27 +0100

    Initial commit

---
+++ Assets/Scripts/Roll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate(){
+        float moveHorizontal = 0.0f;
+        float moveVertical = 0.5f;
+        
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ Assets/Scripts/Roll2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roll2 : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        if (Input.GetKeyDown("space")){
+            transform.Translate(Vector3.up * 25 * Time.deltaTime, Space.World);
+        
+        }
+        
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+        
+        rb.AddForce(movement * speed);
+    }
+
+}
